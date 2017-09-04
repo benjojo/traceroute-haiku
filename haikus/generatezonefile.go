c37e7bf5c29fd20c4d7ad5d624ea1f2c3d1b2931
Ben Cartwright-Cox <Ben@Benjojo.co.uk>
2017-09-05 00:45:54 +0100

    Complete the reverse dns zone file generation

--- haikus/generatezonefile.go
+++ haikus/generatezonefile.go
@@ -13,7 +13,6 @@ type haiku struct {
 }
 
 func main() {
-	fmt.Print(".")
 	files, err := ioutil.ReadDir("./data/")
 	if err != nil {
 		log.Fatalf("Unable to read data dir: %s", err.Error())
@@ -39,12 +38,17 @@ func main() {
 			if insidehaiku && strings.HasPrefix(line, "   [") {
 				insidehaiku = false
 				Author := line[7:]
+				HaikuContent = strings.Trim(HaikuContent, "\n ")
+
 				nhaiku := haiku{
 					Author:  Author,
 					Content: HaikuContent,
 				}
+				if len(strings.Split(HaikuContent, "\n")) == 3 {
+					haikus = append(haikus, nhaiku)
+				}
 				HaikuContent = ""
-				haikus = append(haikus, nhaiku)
+
 				continue
 			}
 
@@ -54,5 +58,34 @@ func main() {
 		}
 	}
 
-	log.Printf("We found %d haikus", len(haikus))
+	log.Printf("# We found %d haikus", len(haikus))
+	var hid uint16 = 1
+	for _, hku := range haikus {
+		// Okay here is the idea, Here is what a normal prefix address looks like:
+		// 0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.c.0.0.0.0.0.5.1.7.0.a.2.ip6.arpa
+		// now we have around 3078 entries to be done here, so we need about ~12 bits
+		// or 3 nibbles to work with, plus the final bit to increment for the letters.
+		// so I will divide address space like so:
+		//
+		//
+		// Z in the increment bit, this goes up with the sentance. X is the haiku ID.
+		// We will always end on 4'th Z nibble, since 3 sentances and 1 author credit.
+		hid++
+		hidhexnibbles := fmt.Sprintf("%04x", hid)
+		sentances := strings.Split(hku.Content, "\n")
+		addrtemplate := fmt.Sprintf(".0.%s.%s.%s.%s.0.0.0.0.0.0.0.0.0.0.0.0.0.0.c.0.0.0.0.0.5.1.7.0.a.2.ip6.arpa.",
+			string(hidhexnibbles[0]), string(hidhexnibbles[1]), string(hidhexnibbles[2]), string(hidhexnibbles[3]))
+		fmt.Printf("0%s\t10\tIN\tPTR\t%s\n", addrtemplate, "haiku-trace.x.benjojo.co.uk.")
+		fmt.Printf("1%s\t10\tIN\tPTR\t%s\n", addrtemplate, dnsfySentance(sentances[0]))
+		fmt.Printf("2%s\t10\tIN\tPTR\t%s\n", addrtemplate, dnsfySentance(sentances[1]))
+		fmt.Printf("3%s\t10\tIN\tPTR\t%s\n", addrtemplate, dnsfySentance(sentances[2]))
+		fmt.Printf("4%s\t10\tIN\tPTR\t%s\n", addrtemplate, dnsfySentance(fmt.Sprintf("author %s", hku.Author)))
+	}
+}
+
+func dnsfySentance(in string) string {
+	in = strings.Replace(in, "\t", "", -1)
+	in = strings.Replace(in, "—", "", -1)
+	in = strings.Replace(in, " ", ".", -1)
+	return strings.Trim(in, ".") + "."
 }
